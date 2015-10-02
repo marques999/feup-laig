87697a07bb248d3acf434693fc3896dff0310aec
Diogo Marques <xmarques999@hotmail.com>
2015-10-02 01:00:18 +0100

    texture minificada

--- laig_proj1/reader/XMLtexture.js
+++ laig_proj1/reader/XMLtexture.js
@@ -1,19 +1,15 @@
-function XMLtexture(path, factorS, factorT) {
-    this.path = path;
-    this.factorS = factorS;
-    this.factorT = factorT;
+function XMLtexture(path, factorS, factorT)
+{
+	this.path = path;
+	this.factorS = factorS;
+	this.factorT = factorT;
 };
 
 XMLtexture.prototype = Object.create(Object.prototype);
 XMLtexture.prototype.constructor = XMLtexture;
 
-XMLtexture.prototype.setFactor = function(factorS, factorT) {
-
-	if (factorS != null) {
-		this.factorS = factorS;
-	}
-	
-	if (factorT == null) {
-		this.factorT = factorT;
-	}
-}
+XMLtexture.prototype.setFactor = function(factorS, factorT)
+{
+	null != factorS && (this.factorS = factorS);
+	null != factorT && (this.factorT = factorT);
+};
