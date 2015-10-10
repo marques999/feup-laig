06e0643b970e605270e253a612aa6753ee399fd8
Diogo Marques <xmarques999@hotmail.com>
2015-10-10 12:32:02 +0100

    construtor civil

--- laig_proj1/reader/MyTriangle.js
+++ laig_proj1/reader/MyTriangle.js
@@ -15,12 +15,22 @@ function MyTriangle(scene, v1, v2, v3) {
 			 		   (v1[2] - v3[2]) * (v1[2] - v3[2]));
 
 	this.c = Math.sqrt((v2[0] - v1[0]) * (v2[0] - v1[0]) + 
-			 		   (v2[1] - v1[1]) * (v2[1] - v1[1]) +
-			 		   (v2[2] - v1[2]) * (v2[2] - v1[2]));
+					   (v2[1] - v1[1]) * (v2[1] - v1[1]) +
+					   (v2[2] - v1[2]) * (v2[2] - v1[2]));
 
-	var angleBeta  = Math.acos((this.a*this.a - this.b*this.b + this.c * this.c) / (2 * this.a * this.c));
+	var angleBeta = Math.acos((this.a * this.a - this.b * this.b + this.c * this.c) / (2 * this.a * this.c));
+	var vertexBminusA = vec3.create();
+	var vertexCminusA = vec3.create();
+
+	this.vertexNormal = vec3.create();
 	this.cosBeta = Math.cos(angleBeta);
 	this.sinBeta = Math.sin(angleBeta);
+
+	vec3.subtract(vertexBminusA, this.v2, this.v1);
+	vec3.subtract(vertexCminusA, this.v3, this.v1);
+	vec3.cross(this.vertexNormal, vertexBminusA, vertexCminusA);
+	vec3.normalize(this.vertexNormal, this.vertexNormal);
+
 	this.initBuffers();
 };
 
@@ -39,19 +49,10 @@ MyTriangle.prototype.initBuffers = function() {
 		0, 1, 2,
 	];
 
-	var vertexBminusA = vec3.create();
-	var vertexCminusA = vec3.create();
-	var vertexNormal = vec3.create();
-	
-	vec3.subtract(vertexBminusA, this.v2, this.v1);
-	vec3.subtract(vertexCminusA, this.v3, this.v1);
-	vec3.cross(vertexNormal, vertexBminusA, vertexCminusA);
-	vec3.normalize(vertexNormal, vertexNormal);
-
 	this.normals = [
-		vertexNormal[0], vertexNormal[1], vertexNormal[2],
-		vertexNormal[0], vertexNormal[1], vertexNormal[2],
-		vertexNormal[0], vertexNormal[1], vertexNormal[2]
+		this.vertexNormal[0], this.vertexNormal[1], this.vertexNormal[2],
+		this.vertexNormal[0], this.vertexNormal[1], this.vertexNormal[2],
+		this.vertexNormal[0], this.vertexNormal[1], this.vertexNormal[2]
 	];
 
 	this.primitiveType = this.scene.gl.TRIANGLES;
