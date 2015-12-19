5711a11864c05b404e4eba3cb67b88a9793d685a
Diogo Marques <xmarques999@hotmail.com>
2015-12-19 13:16:50 +0000

    constantes mágicas

--- laig_proj3/reader/duplohex/pieceController.js
+++ laig_proj3/reader/duplohex/pieceController.js
@@ -1,25 +1,40 @@
 /**
- @author Carlos Samouco, Diogo Marques
+ * construtor default da classe 'PieceController'
+ * @constructor
+ * @author Carlos Samouco, Diogo Marques
+ * @param {XMLscene} scene - XMLscene onde esta primitiva será desenhada
+ * @param {GameBoard} board - qwertyuiop asdfghjkl zxcvbnm
+ * @param {Object} player1 - qwertyuiop asdfghjkl zxcvbnm
+ * @param {Object} player2 - qwertyuiop asdfghjkl zxcvbnm
+ * @return {null}
 */
 function PieceController(scene, board, player1, player2) {
-
+	//--------------------------------------------------------	
 	MyPrimitive.call(this, scene);
 	//--------------------------------------------------------	
 	this.numberDiscs = 24;
 	this.numberRings = 24;
+	this.numberStacks = 8;
 	//--------------------------------------------------------	
-	this.p1Discs = player1.discs;
-	this.p1Rings = player1.rings;
-	this.p2Discs = player2.discs;
-	this.p2Rings = player2.rings;
+	this.horizontalSpace = 2.1;
+	this.verticalSpace = 0.9;
+	this.stackSpace = 5.0;
+	//--------------------------------------------------------	
 	this.player1 = player1;
 	this.player2 = player2;
 	//--------------------------------------------------------	
 	this.gameBoard = board;
+	this.defaultAngle = 2 * Math.cos(Math.PI / 6) / 3;
 	this.selectedPiece = null;
 	this.baseSize = board.baseSize;
 	this.basePos = board.basePos;
 	this.boxPos = board.boxPos;
+	//--------------------------------------------------------	
+	this.defaultScale = [
+		this.baseSize[0] * 1.5 / 5.0, 
+		this.baseSize [1] * 1.3 / 5.0, 
+		this.baseSize[0] * 1.5 / 5.0
+	];
 	//--------------------------------------------------------	
 	this.pieces = [];
 	this.p1RingStacks = [];
@@ -29,17 +44,15 @@ function PieceController(scene, board, player1, player2) {
 	//--------------------------------------------------------	
 	this.initialize();
 };
-
+//--------------------------------------------------------	
 PieceController.prototype = Object.create(MyPrimitive.prototype);
 PieceController.prototype.constructor = PieceController;
-
+//--------------------------------------------------------	
 PieceController.prototype.generateRandom = function() {
-	var randomDirection = Math.random() < 0.5 ? 1 : -1;
-	return randomDirection * Math.random() * 0.06;
-}
-
+	return (Math.random() < 0.5 ? 1 : -1) * Math.random() * 0.06;
+};
+//--------------------------------------------------------	
 PieceController.prototype.initialize = function() {
-
 	//--------------------------------------------------------	
 	this.p1Discs_start = 0;
 	this.p1Discs_end = this.p1Discs_start + this.player1.discs;
@@ -53,7 +66,7 @@ PieceController.prototype.initialize = function() {
 	//--------------------------------------------------------	
 	for (var i = this.p1Discs_start; i < this.p1Discs_end; i++) {
 
-		var nStack = i % 8;
+		var nStack = i % this.numberStacks;
 
 		if (this.p1DiscStacks[nStack] == undefined) {
 			this.p1DiscStacks[nStack] = [];
@@ -61,9 +74,9 @@ PieceController.prototype.initialize = function() {
 
 		this.pieces[i] = new ObjectDisc(this.scene, this.player1.color,
 		[
-			(nStack % 2) * 2.1 + this.generateRandom() + this.boxPos[0],
-			this.p1DiscStacks[nStack].length * 1.0 + this.boxPos[1],
-			Math.floor(nStack / 2) * 2.1 + this.generateRandom() + this.boxPos[2]
+			(nStack % 2) * this.horizontalSpace + this.generateRandom() + this.boxPos[0],
+			this.p1DiscStacks[nStack].length + this.boxPos[1],
+			Math.floor(nStack / 2) * this.horizontalSpace + this.generateRandom() + this.boxPos[2]
 		]);
 
 		this.p1DiscStacks[nStack].push(i);
@@ -71,7 +84,7 @@ PieceController.prototype.initialize = function() {
 	//--------------------------------------------------------	
 	for (var i = this.p1Rings_start; i < this.p1Rings_end; i++) {
 
-		var nStack = (i - this.p1Rings_start) % 8;
+		var nStack = (i - this.p1Rings_start) % this.numberStacks;
 
 		if (this.p1RingStacks[nStack] == undefined) {
 			this.p1RingStacks[nStack] = [];
@@ -79,9 +92,9 @@ PieceController.prototype.initialize = function() {
 
 		this.pieces[i] = new ObjectRing(this.scene, this.player1.color,
 		[
-			(nStack % 2) * 2.1 + this.generateRandom() + 5 + this.boxPos[0],
-			this.p1RingStacks[nStack].length * 0.9 + this.boxPos[1],
-			Math.floor(nStack / 2) * 2.1 + this.generateRandom() + this.boxPos[2]
+			(nStack % 2) * this.horizontalSpace + this.generateRandom() + this.stackSpace + this.boxPos[0],
+			this.p1RingStacks[nStack].length * this.verticalSpace + this.boxPos[1],
+			Math.floor(nStack / 2) * this.horizontalSpace + this.generateRandom() + this.boxPos[2]
 		]);
 
 		this.p1RingStacks[nStack].push(i);
@@ -89,7 +102,7 @@ PieceController.prototype.initialize = function() {
 	//--------------------------------------------------------	
 	for (var i = this.p2Discs_start; i < this.p2Discs_end; i++) {
 
-		var nStack = (i - this.p2Discs_start) % 8;
+		var nStack = (i - this.p2Discs_start) % this.numberStacks;
 
 		if (this.p2DiscStacks[nStack] == undefined) {
 			this.p2DiscStacks[nStack] = [];
@@ -97,9 +110,9 @@ PieceController.prototype.initialize = function() {
 
 		this.pieces[i] = new ObjectDisc(this.scene, this.player2.color,
 		[
-			-((nStack % 2) * 2.1 + this.generateRandom() + this.boxPos[0]),
-			+(this.p2DiscStacks[nStack].length * 1.0 + this.boxPos[1]),
-			-(Math.floor(nStack / 2) * 2.1 + this.generateRandom() + this.boxPos[2])
+			-((nStack % 2) * this.horizontalSpace + this.generateRandom() + this.boxPos[0]),
+			+(this.p2DiscStacks[nStack].length + this.boxPos[1]),
+			-(Math.floor(nStack / 2) * this.horizontalSpace + this.generateRandom() + this.boxPos[2])
 		]);
 
 		this.p2DiscStacks[nStack].push(i);
@@ -107,7 +120,7 @@ PieceController.prototype.initialize = function() {
 	//--------------------------------------------------------	
 	for (var i = this.p2Rings_start; i < this.p2Rings_end; i++) {
 
-		var nStack = (i - this.p2Rings_start) % 8;
+		var nStack = (i - this.p2Rings_start) % this.numberStacks;
 
 		if (this.p2RingStacks[nStack] == undefined) {
 			this.p2RingStacks[nStack] = [];
@@ -115,19 +128,19 @@ PieceController.prototype.initialize = function() {
 
 		this.pieces[i] = new ObjectRing(this.scene, this.player2.color,
 		[
-			-((nStack % 2) * 2.1 + this.generateRandom() + 5 + this.boxPos[0]),
-			+(this.p2RingStacks[nStack].length * 0.9 + this.boxPos[1]),
-			-(Math.floor(nStack / 2) * 2.1 + this.generateRandom() + this.boxPos[2])
+			-((nStack % 2) * this.horizontalSpace + this.generateRandom() + this.stackSpace + this.boxPos[0]),
+			+(this.p2RingStacks[nStack].length * this.verticalSpace + this.boxPos[1]),
+			-(Math.floor(nStack / 2) * this.horizontalSpace + this.generateRandom() + this.boxPos[2])
 		]);
 
 		this.p2RingStacks[nStack].push(i);
 	}
-}
-
+};
+//--------------------------------------------------------	
 PieceController.prototype.display = function() {
 
 	this.scene.pushMatrix();
-	this.scene.scale(this.baseSize[0]*1.5/5.0, this.baseSize[1]*1.3/5.0, this.baseSize[0]*1.5/5.0);
+	this.scene.scale(this.defaultScale[0], this.defaultScale[1], this.defaultScale[2]);
 
 	for (var i = 0; i < this.pieces.length; i++) {
 
@@ -141,7 +154,7 @@ PieceController.prototype.display = function() {
 			this.scene.translate(piecePosition[0], piecePosition[1], piecePosition[2]);
 		}
 
-		this.scene.rotate(-Math.PI/2, 1.0, 0.0, 0.0);
+		this.scene.rotate(Math.PI/2, -1.0, 0.0, 0.0);
 		this.scene.registerPicking(this.pieces[i]);
 		this.pieces[i].display();
 		this.scene.popMatrix();
@@ -149,12 +162,12 @@ PieceController.prototype.display = function() {
 	
 	this.scene.popMatrix();
  };
-
- PieceController.prototype.removeFromStack = function(pieceId, x, y) {
- 	//--------------------------------------------------------	
+//--------------------------------------------------------
+PieceController.prototype.removeFromStack = function(pieceId, x, y) {
+	//--------------------------------------------------------
 	if (pieceId < this.p1Discs_end) {
 
-		var nStack = pieceId % 8;
+		var nStack = pieceId % this.numberStacks;
 		var top = this.p1DiscStacks[nStack].length - 1;
 
 		if (this.p1DiscStacks[nStack][top] == pieceId) {
@@ -166,10 +179,10 @@ PieceController.prototype.display = function() {
 			this.selectedPiece = null;
 		}
 	}
-	//--------------------------------------------------------	
+	//--------------------------------------------------------
 	else if (pieceId >= this.p1Rings_start && pieceId < this.p1Rings_end){
 
-		var nStack = (pieceId - this.p1Rings_start) % 8;
+		var nStack = (pieceId - this.p1Rings_start) % this.numberStacks;
 		var top = this.p1RingStacks[nStack].length - 1;
 
 		if (this.p1RingStacks[nStack][top] == pieceId) {
@@ -181,10 +194,10 @@ PieceController.prototype.display = function() {
 			this.selectedPiece = null;
 		}
 	}
-	//--------------------------------------------------------	
+	//--------------------------------------------------------
 	else if (pieceId >= this.p2Discs_start && pieceId < this.p2Discs_end){
 
-		var nStack = (pieceId - this.p2Discs_start) % 8;
+		var nStack = (pieceId - this.p2Discs_start) % this.numberStacks;
 		var top = this.p2DiscStacks[nStack].length - 1;
 
 		if (this.p2DiscStacks[nStack][top] == pieceId) {
@@ -196,10 +209,10 @@ PieceController.prototype.display = function() {
 			this.selectedPiece = null;
 		}
 	}
-	//--------------------------------------------------------	
+	//--------------------------------------------------------
 	else if (pieceId >= this.p2Rings_start && pieceId < this.p2Rings_end) {
 
-		var nStack = (pieceId - this.p2Rings_start) % 8;
+		var nStack = (pieceId - this.p2Rings_start) % this.numberStacks;
 		var top = this.p2RingStacks[nStack].length - 1;
 
 		if (this.p2RingStacks[nStack][top] == pieceId) {
@@ -212,52 +225,52 @@ PieceController.prototype.display = function() {
 		}
 	}
 };
-
+//--------------------------------------------------------
 PieceController.prototype.placePiece = function(pieceId, x, y)  {
-	
 	//--------------------------------------------------------	
 	var piece = this.pieces[pieceId];
-
+	//--------------------------------------------------------	
 	if (!piece.wasPlaced()) {
 		this.removeFromStack(pieceId, x, y);		
 	}
-
+	//--------------------------------------------------------	
 	piece.setColor('default');
-
-	var x2 = (this.baseSize[0]/2 + this.baseSize[0]/4)*2/3*x +  this.baseSize[0]*(this.basePos[0]/1.5);
-	var y2 = this.basePos[1];
-	var z2 = -(this.baseSize[1]/2)*2/3*x*Math.cos(30*Math.PI/180) - this.baseSize[1]*y*2/3*Math.cos(30*Math.PI/180) - this.baseSize[1]*(this.basePos[2]/1.5);
+	//--------------------------------------------------------	
+	var newX = this.baseSize[0] * 0.5 * x + this.baseSize[0] * this.basePos[0] / 1.5;
+	var newY = this.basePos[1];
+	var newZ = -(this.baseSize[1] * 0.5) * this.defaultAngle * x - this.baseSize[1] * this.defaultAngle * y - this.baseSize[1] * this.basePos[2] / 1.5;
+	//--------------------------------------------------------	
 	var piecePosition = this.pieces[pieceId].position;
-	var dist = vec3.dist([x2, y2 + 6, z2], piecePosition);
-
-	this.animation = new LinearAnimation(dist*0.20,
+	var pieceDistance = vec3.dist([newX, newY + 6, newZ], piecePosition);
+	//--------------------------------------------------------	
+	this.animation = new LinearAnimation(pieceDistance * 0.2,
 	[
 		piecePosition, 
 		[piecePosition[0], 3.0, piecePosition[2]],
-		[x2, y2 + 3, z2], 
-		[x2, y2, z2]
+		[newX, newY + 3, newZ], 
+		[newX, newY, newZ]
 	]);
-
+	//--------------------------------------------------------	
 	this.animationId = pieceId;
 	this.animation.start();
-
-	piece.setPosition(x2, y2, z2);
+	//--------------------------------------------------------	
+	piece.setPosition(newX, newY, newZ);
 	piece.setColor('default');
 	piece.place(x, y);
-}
-
+};
+//--------------------------------------------------------	
 PieceController.prototype.pieceAt = function(pieceId) {
 	return this.pieces[pieceId];
 };
-
+//--------------------------------------------------------	
 PieceController.prototype.isDisc = function(pieceId) {
 	return (pieceId >= this.p1Discs_start && pieceId < this.p1Discs_end) || (pieceId >= this.p2Discs_start && pieceId < this.p2Discs_end);
 };
-
+//--------------------------------------------------------	
 PieceController.prototype.isRing = function(pieceId) {
 	return (pieceId >= this.p1Rings_start && pieceId < this.p1Rings_end) || (pieceId >= this.p2Rings_start && pieceId < this.p2Rings_end);
 };
-
+//--------------------------------------------------------	
 PieceController.prototype.update = function(delta) {
 
 	if (this.animationId == null) {
@@ -274,7 +287,7 @@ PieceController.prototype.update = function(delta) {
 
 	return false;
 };
-
+//--------------------------------------------------------	
 PieceController.prototype.unselectActivePiece = function() {
 
 	if (this.selectedPiece != null && this.selectedPiece < this.pieces.length) {
@@ -282,26 +295,22 @@ PieceController.prototype.unselectActivePiece = function() {
 	}
 
 	this.selectedPiece = null;
-}
-
+};
+//--------------------------------------------------------	
 PieceController.prototype.selectPiece = function(pickingId) {
 
-	/*if(this.animation != null && this.animation.active) {
-		return null;
-	}*/
+	var boardSize = this.gameBoard.numberCells;
 
-	var boarSize = this.gameBoard.numberRows*this.gameBoard.numberColumns;
-
-	if (pickingId >= boarSize + 1 && pickingId <= boarSize + this.pieces.length) {
+	if (pickingId >= boardSize + 1 && pickingId <= boardSize + this.pieces.length) {
 		
-		var id = pickingId - boarSize - 1;
+		var id = pickingId - boardSize - 1;
 
 		if (this.selectedPiece == id) {
 			this.unselectActivePiece();
 			return -1;
 		}
 
-		if (this.selectedPiece == null){
+		if (this.selectedPiece == null) {
 			this.selectedPiece = id;
 		}
 		else {
@@ -309,8 +318,8 @@ PieceController.prototype.selectPiece = function(pickingId) {
 		}
 
 		if (id < this.p1Discs_end) {
-			// player1 disc
-			var nStack = id % 8;
+
+			var nStack = id % this.numberStacks;
 			var top = this.p1DiscStacks[nStack].length - 1;
 
 			if (this.p1DiscStacks[nStack][top] == id || this.p1DiscStacks[nStack].indexOf(id) == -1) {
@@ -320,8 +329,8 @@ PieceController.prototype.selectPiece = function(pickingId) {
 			}
 		}
 		else if (id >= this.p1Rings_start && id < this.p1Rings_end){
-			// player 1 ring
-			var nStack = (id - this.p1Rings_start) % 8;
+
+			var nStack = (id - this.p1Rings_start) % this.numberStacks;
 			var top = this.p1RingStacks[nStack].length - 1;
 
 			if (this.p1RingStacks[nStack][top] == id || this.p1RingStacks[nStack].indexOf(id) == -1) {
@@ -331,8 +340,8 @@ PieceController.prototype.selectPiece = function(pickingId) {
 			}
 		}
 		else if (id >= this.p2Discs_start && id < this.p2Discs_end) {
-			// player 2 discs
-			var nStack = (id - this.p2Discs_start) % 8;
+
+			var nStack = (id - this.p2Discs_start) % this.numberStacks;
 			var top = this.p2DiscStacks[nStack].length - 1;
 
 			if (this.p2DiscStacks[nStack][top] == id || this.p2DiscStacks[nStack].indexOf(id) == -1) {
@@ -342,11 +351,11 @@ PieceController.prototype.selectPiece = function(pickingId) {
 			}
 		}
 		else {
-			// player 2 rings
-			var nStack = (id - this.p2Rings_start) % 8;
+
+			var nStack = (id - this.p2Rings_start) % this.numberStacks;
 			var top = this.p2RingStacks[nStack].length - 1;
 
-			if(this.p2RingStacks[nStack][top] == id || this.p2RingStacks[nStack].indexOf(id) == -1) {
+			if (this.p2RingStacks[nStack][top] == id || this.p2RingStacks[nStack].indexOf(id) == -1) {
 				this.selectedPiece = id;
 				this.pieces[id].setColor('yellow');			
 				return id;
@@ -355,6 +364,7 @@ PieceController.prototype.selectPiece = function(pickingId) {
 
 		this.selectedPiece = id;
 		this.pieces[id].setColor('red');
+
 		return -1;
 	}
 };
